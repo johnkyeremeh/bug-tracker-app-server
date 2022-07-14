c864cbf9210603f9d51586d85775ea296b3635c9
John Kyeremeh <john.a.kyeremeh@gmail.com>
2022-07-13 21:33:07 -0700

    Updated method to say get_current_user instead of auto_login for clarity

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,36 +1,35 @@
 class Api::V1::SessionsController < ApplicationController
 
-        def create 
-            user = User.find_by(username: params[:session][:username])
-          
-            if user && user.authenticate(params[:session][:password])
-                session[:user_id] = user.id
-                render json: {
-                    user: UserSerializer.new(user),
-                    status: 201,
-                    success: "Welcome back, #{user.username}!"}
-            else 
-                render json: { message: "Unable to find a user with that email or pasword"}, status: 500
-            end
-        end
+    
+    def create 
+    
+        user = User.find_by(username: params[:session][:username])
       
-
-        def auto_login
-            if logged_in?
-                render json: {user: UserSerializer.new(current_user)}
-            else 
-                render json: {errors: "No user logged in."}
-            end
-        end
-
-        def destroy 
-            session.clear 
+        if user && user.authenticate(params[:session][:password])
+            session[:user_id] = user.id
             render json: {
-                error: "sucessfully logged out"}
+                user: UserSerializer.new(user),
+                status: 201,
+                success: "Welcome back, #{user.username}!"}
+        else 
+            render json: { message: "Unable to find a user with that email or pasword"}, status: 500
         end
-        
-        def user_is_authed
-            render json: {message: "You are authorized"}
-          end
-
     end
+  
+
+    def get_current_user
+        if logged_in?
+            render json: {user: UserSerializer.new(current_user)}
+        else 
+            render json: {errors: "No user logged in."}
+        end
+    end
+
+    def destroy 
+        session.clear 
+        render json: {
+            error: "sucessfully logged out"}
+    end
+    
+
+end

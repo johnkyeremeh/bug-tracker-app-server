f0ca393c9db02c63717ee0d183c296ffb28831a3
John Kyeremeh <john.a.kyeremeh@gmail.com>
2022-07-19 22:05:15 -0700

    Uncommented information within model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,9 @@ class User < ApplicationRecord
    # adds methods to set and authenticate against BCrypt password. #.authenticate(password)
     has_secure_password
     
-    # has_many :projects
+    has_many :projects
     has_many :bugs
-    # accepts_nested_attributes_for :projects, reject_if: :all_blank
+    accepts_nested_attributes_for :projects, reject_if: :all_blank
     accepts_nested_attributes_for :bugs, reject_if: :all_blank
     
     # Ensure each user is unique username and unique email

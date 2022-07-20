a74ef972cac20a46950e029f2f44b9f89660921b
John Kyeremeh <john.a.kyeremeh@gmail.com>
2022-07-19 22:04:15 -0700

    Uncommented information

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,5 @@
-# class Project < ApplicationRecord
-#     belongs_to :user
-#     has_many :bugs    
-#     accepts_nested_attributes_for :bugs, reject_if: :all_blank
-# end
+class Project < ApplicationRecord
+    belongs_to :user
+    has_many :bugs    
+    accepts_nested_attributes_for :bugs, reject_if: :all_blank
+end

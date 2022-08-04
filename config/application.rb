458754b80dad5878ae0e959f0b7ac33ef220ea68
John Kyeremeh <john.a.kyeremeh@gmail.com>
2022-08-04 15:52:38 -0700

    Added congif.autoloaer to classic mode in application.rb

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,7 @@ module BugTrackerApi
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
+    config.autoloader = :classic
 
     # config.autoload_paths << Rails.root.join('lib')
     # Configuration for the application, engines, and railties goes here.

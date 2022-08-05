f901e1e9a8a452e8e698364eb4a506c215bafd21
John Kyeremeh <john.a.kyeremeh@gmail.com>
2022-08-05 00:55:57 -0700

    Updated cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://leafy-bunny-662da6.netlify.app'
+    # origins ['http://localhost:3001', 'https://leafy-bunny-662da6.netlify.app']
+    origins '*'
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],

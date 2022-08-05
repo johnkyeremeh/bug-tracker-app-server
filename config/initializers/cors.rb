a87c57d3899dc2025f4fa7df7cfd55480c308a8b
John Kyeremeh <john.a.kyeremeh@gmail.com>
2022-08-05 09:15:11 -0700

    Updated CORS removed credentials = true

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,11 +7,9 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    # origins ['http://localhost:3001', 'https://leafy-bunny-662da6.netlify.app']
-    origins '*'
+    origins  'https://leafy-bunny-662da6.netlify.app'
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
-      credentials: true 
     end
 end
